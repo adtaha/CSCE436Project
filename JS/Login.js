d915985542fb93cbed7267519351da30f4c3ec15
jhassmann <jordan0465@icloud.com>
2023-04-21 20:31:48 -0500

    Login Page

---
+++ JS/Login.js
@@ -0,0 +1,13 @@
+
+
+const username = document.querySelector('#username')
+const password = document.querySelector('#password')
+const loginBtn = document.querySelector('#loginBtn')
+const form     = document.querySelector('#login-form')
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    if (username.value && password.value) {
+        window.location = '/home.html'
+    }
+})
