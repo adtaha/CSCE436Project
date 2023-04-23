9d9b9c02014feb216c1457cadbbea73c7481c054
Reed Billedo <rjbilledo@tamu.edu>
2023-04-23 01:39:25 -0500

    added text to speech loop

--- JS/TextToSpeech.js
+++ JS/TextToSpeech.js
@@ -1,11 +1,13 @@
 
 // Get the announce button
 const announceBtn = document.querySelector('#announce')
+const emotions = [['happy', 'smiling', 'positive'], ['bored'], ['frowning', 'sad', 'negative'], ['angry', 'mad', 'negative']]
 
+let i = 0
 // Add click event listener
 announceBtn.addEventListener('click', e => {
-    const emotions = ['happy', 'smiling', 'positive']
-    emotions.forEach(emotion => tts(emotion))
+    emotions[i].forEach(emotion => tts(emotion))
+    i += 1
 })
 
 
@@ -14,7 +16,7 @@ function tts(speechText) {
     var speech = new SpeechSynthesisUtterance();
     speech.lang = "en-US";
     speech.text = speechText;
-  
+
     window.speechSynthesis.speak(speech);
 }
 
